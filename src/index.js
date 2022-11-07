25767549d6549aa5684eb16089d6593ae43c7a2a
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-11-07 09:33:01 +0530

    mongo connection

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-//require("dotenv").config();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = require("express")();
 const cors = require("cors");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const dev = true;
 
 app.use(cors());
 app.use(morgan("dev"));
+
+// connect mongodb
+mongoose.connect(
+  dev ? process.env.MONGO_LOCAL : process.env.MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Connected to MongoDB");
+  }
+);
 
 app.get("/", (req, res) => {
   res.send("Hello");
