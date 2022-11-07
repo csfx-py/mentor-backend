2dcc3d3277d38b7b53bbcccb1c0096ba5eff7df7
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-11-07 14:04:36 +0530

    user schema

---
+++ src/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+// User schema
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  Posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+  role: {
+    type: String,
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
