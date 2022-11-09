280c7aed6225ab7f02cbf7797f515ffe8ea35b2c
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-11-09 09:38:34 +0530

    create token util

---
+++ src/utils/createToken.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (user, secret, exp) => {
+  const { _id, name, role } = user;
+  return jwt.sign(
+    {
+      _id,
+      name,
+      role
+    },
+    secret,
+    { expiresIn: exp }
+  );
+};
