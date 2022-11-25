3106d5453056e84d26d668efa166daa1af8cafcb
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-11-25 10:14:40 +0530

    middleware to verify user

--- src/utils/verifyUser.js
+++ src/utils/verifyUser.js
@@ -1,16 +1,29 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 module.exports = (req, res, next) => {
-  const token = req.header("auth");
-  if (!token) return res.status(401).send("Unauthorized");
-
   try {
-    const { role } = jwt.verify(token, process.env.ACCESS_TOKEN_SEC);
+    const token = req.cookies.token;
+    if (!token) return res.status(401).send("Unauthorized");
 
-    if (!role) res.status(401).send("Unauthorized");
+    const { _id, role } = jwt.verify(token, process.env.ACCESS_TOKEN_SEC);
+
+    const user = User.findById(_id);
+    if (!user) return res.status(401).send("Unauthorized");
+
+    if (!role)
+      res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+
+    req.reqUser = { _id, role };
 
     next();
   } catch (err) {
-    res.status(401).send("Unauthorized. Error: " + err);
+    res.status(401).json({
+      success: false,
+      message: "Unauthorized" + err.message,
+    });
   }
 };
