ff01db12c18092875f02515219dab61501810b46
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-12-11 23:56:20 +0530

    tags hooks

--- src/models/Tag.js
+++ src/models/Tag.js
@@ -14,4 +14,39 @@ const TagSchema = new mongoose.Schema({
   },
 });
 
+// pre delete middleware
+TagSchema.pre("deleteOne", async function (next) {
+  try {
+    const tag = await this.model.findOne(this.getQuery());
+
+    const Post = require("./Post");
+    await Post.updateMany(
+      {
+        tags: tag._id,
+      },
+      {
+        $pull: {
+          tags: tag._id,
+        },
+      }
+    );
+
+    const User = require("./User");
+    await User.updateMany(
+      {
+        followingTags: tag._id,
+      },
+      {
+        $pull: {
+          followingTags: tag._id,
+        },
+      }
+    );
+
+    next();
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = mongoose.model("Tag", TagSchema);
