a2cd3717f2c4d2ed85997e29b25caf0b9cf2e149
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-12-12 00:04:51 +0530

    pure function to get user data

---
+++ src/utils/getUserReturnInfo.js
@@ -0,0 +1,11 @@
+module.exports = (userDoc) => {
+  return {
+    _id: userDoc._id,
+    name: userDoc.name,
+    email: userDoc.email,
+    followingTags: userDoc.followingTags,
+    posts: userDoc.posts,
+    avatar: userDoc.avatar,
+    role: userDoc.role,
+  };
+};
