aac6003ae0133388a104287b232d41919df3f352
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-12-15 12:45:20 +0530

    verify admin middleware

--- src/utils/verifyAdmin.js
+++ src/utils/verifyAdmin.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const token = req.header("auth");
+  const token = req.cookies.token;
   if (!token) return res.status(401).send("Unauthorized");
 
   try {
